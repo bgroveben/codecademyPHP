d001be6fc482946c6842887bc0c534f2514b862c
bgrove1974 <bgroveben@gmail.com>
2016-04-20 06:40:57 -0700

    If statements

---
+++ conditionalsAndControlFlow/index.php
@@ -0,0 +1,15 @@
+<html>
+  <head>
+    <title>If statements</title>
+  </head>
+  <body>
+    <p>
+      <?php
+        $items = 12;
+        if($items > 5) {
+            echo "You get a 10% discount!";
+        }
+      ?>
+    </p>
+  </body>
+</html>
