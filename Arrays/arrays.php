7d770f27fa77b0229e8b2a2a8aaf28ba9b159ab7
bgrove1974 <bgroveben@gmail.com>
2016-04-21 04:07:45 -0700

    PHP Arrays

--- Arrays/arrays.php
+++ Arrays/arrays.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
-<html>
+<html lang="en-US">
     <head>
+        <meta charset="UTF-8" />
         <title></title>
     </head>
     <body>
         <?php
-
+            $array = array("Egg", "Tomato", "Beans");
+            echo $array[0] . ', ' .$array[1] . ', ' . $array[2];
          ?>
     </body>
 </html>
