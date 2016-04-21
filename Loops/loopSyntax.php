c8367a75e1c9f42178d0856e7427a68367eb578d
bgrove1974 <bgroveben@gmail.com>
2016-04-21 16:41:17 -0700

    Loop syntax

---
+++ Loops/loopSyntax.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en-US">
+    <head>
+        <meta charset="UTF-8" />
+        <title></title>
+    </head>
+    <body>
+        <?php
+            for ($i = 2; $i < 11; $i += 2) {
+                echo "<p>$i</p>";
+            }
+         ?>
+    </body>
+</html>
