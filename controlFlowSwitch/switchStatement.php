2697a3dd64b4f3ad7afdf54f0ad149f3777f589e
bgrove1974 <bgroveben@gmail.com>
2016-04-20 18:01:28 -0700

    More switch statements...because they're fun.

--- controlFlowSwitch/switchStatement.php
+++ controlFlowSwitch/switchStatement.php
@@ -4,6 +4,8 @@
         <title>Switch Statement</title>
     </head>
     <body>
+        <p>Switch Statement the first:
+        <br />
         <?php
         switch (1) {
             case 0:
@@ -19,5 +21,44 @@
                 echo 'The value is not 0, 1, or 2';
         }
          ?>
+        </p>
+        <p>Switch Statement the second:
+        <br />
+        <?php
+        $myNum = 2;
+        switch($myNum) {
+            case 1:
+                echo "1";
+                break;
+            case 2:
+                echo "2";
+                break;
+            case 3:
+                echo "3";
+                break;
+            default:
+                echo "None of the above";
+        }
+         ?>
+        </p>
+        <p>Switch Statement the third:
+        <br />
+        <?php
+        $fruit = "Jabuticaba";
+        switch ($fruit) {
+            case 'Apple':
+                echo "Yummy.";
+                break;
+            case 'Orange':
+                echo "Tangy.";
+                break;
+            case 'Jabuticaba':
+                echo "What's a jabuticaba?";
+                break;
+            default:
+                echo "No fruit for you.";
+        }
+         ?>
+        </p>
     </body>
 </html>
