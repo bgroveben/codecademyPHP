0edfeb71aab00e37097b71360b28f3d8bde8af5d
bgrove1974 <bgroveben@gmail.com>
2016-04-21 18:54:44 -0700

    More for loops? Hell Yes

---
+++ Loops/myForLoop.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en-US">
+    <head>
+        <meta charset="UTF-8" />
+        <title>First For Loop</title>
+    </head>
+    <body>
+        <?php
+            for ($i = 10; $i < 101; $i += 10) {
+                echo "<p>$i</p>";
+            }
+         ?>
+    </body>
+</html>
