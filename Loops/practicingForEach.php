52ee262199d67c4db4546d54f21ed11fbca86e2a
bgrove1974 <bgroveben@gmail.com>
2016-04-22 01:24:08 -0700

    Practicing with ForEach

---
+++ Loops/practicingForEach.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en-US">
+    <head>
+        <meta charset="UTF-8" />
+        <title>Practicing with ForEach</title>
+    </head>
+    <body>
+        <?php
+            $sentence = array("I'm ", "learning ", "PHP!");
+            foreach ($sentence as $word) {
+                echo $word;
+            }
+         ?>
+    </body>
+</html>
