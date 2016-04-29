de3f920969e8ea2b24f3b53486eb60a06478510a
bgrove1974 <bgroveben@gmail.com>
2016-04-28 18:52:10 -0700

    Length function

---
+++ builtInFunctions/length.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en-US">
+    <head>
+        <meta charset="UTF-8" />
+        <title></title>
+    </head>
+    <body>
+        <p><?php
+        $length = strlen("Nebuchadnezzar");
+        print "Nebuchadnezzar has $length letters";
+        ?></p>
+    </body>
+</html>
