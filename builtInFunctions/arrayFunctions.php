772038a22f52c42f4d1054620f1a42a643960efe
bgrove1974 <bgroveben@gmail.com>
2016-05-20 00:57:46 -0700

    Array Functions part 2

--- builtInFunctions/arrayFunctions.php
+++ builtInFunctions/arrayFunctions.php
@@ -19,5 +19,21 @@
         print count($programming_languages);
         ?>
         </p>
+        <p>
+    	<?php
+    	// Create an array with several elements in it,
+    	// then sort it and print the joined elements to the screen
+        $the_array = array(3, 1, 4, 1, 5, 9);
+        sort($the_array);
+        print join(",", $the_array);
+    	?>
+    	</p>
+    	<p>
+    	<?php
+    	// Reverse sort your array and print the joined elements to the screen
+        rsort($the_array);
+        print join(",", $the_array);
+    	?>
+    	</p>
     </body>
 </html>
